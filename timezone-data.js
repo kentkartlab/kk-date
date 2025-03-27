ceff7e278af6fc2215d2a767d5cb0d8a5a96e413
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2025-03-27 21:11:42 +0500

    Update timezone-data.js

--- timezone-data.js
+++ timezone-data.js
@@ -1,10 +1,10 @@
 const timezoneData = {
-	'UTC': { standardOffset: 0, dstOffset: 0, dstRule: 'none' },
-	'GMT': { standardOffset: 0, dstOffset: 0, dstRule: 'none' },
-  'Asia/Qatar': { standardOffset: 3 * 3600, dstOffset: 0 },
-  'Asia/Kuala_Lumpur': { standardOffset: 8 * 3600, dstOffset: 0 },
-  'Europe/Istanbul': { standardOffset: 3 * 3600, dstOffset: 2 * 3600 },
-  'Asia/Yekaterinburg': { standardOffset: 5 * 3600, dstOffset: 0 },
-}
+	UTC: { standardOffset: 0, dstOffset: 0, dstRule: 'none' },
+	GMT: { standardOffset: 0, dstOffset: 0, dstRule: 'none' },
+	'Europe/Istanbul': { standardOffset: 3 * 3600, dstOffset: 2 * 3600 },
+	'Asia/Qatar': { standardOffset: 3 * 3600, dstOffset: 0 },
+	'Asia/Kuala_Lumpur': { standardOffset: 8 * 3600, dstOffset: 0 },
+	'Asia/Yekaterinburg': { standardOffset: 5 * 3600, dstOffset: 0 },
+};
 
 module.exports = timezoneData;
